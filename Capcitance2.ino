45d399e8723912ab24cf7afc0ccae48eefb6e3bb
ueki815 <115120789+ueki815@users.noreply.github.com>
2023-02-17 15:10:43 -0500

    Add files via upload
    when you step on the sensor, the capacitance about doubles. however, sometimes the starting value is different (starts at 5k pF and doubles to 10k, other times starts at 10k and doubles to 20k) why is this? does it matter? could write code so that instead of above a threshold value, it's when the capacitance changes significantly?

--- Capcitance2.ino
+++ Capcitance2.ino
@@ -1,57 +1,65 @@
-//https://www.instructables.com/Measure-Capacitance-with-Arduino/
-//https://docs.arduino.cc/tutorials/generic/capacitance-meter
+/* code adapted from https://www.instructables.com/Measure-Capacitance-with-Arduino/
+ https://docs.arduino.cc/tutorials/generic/capacitance-meter
+
+*/
 
 // Initialize Pins
-int analogPin = 0;
+int analogPin = A0;
 int chargePin = 13;
 int dischargePin = 11; //speeds up discharging process, not necessary though
 
 // Initialize Resistor
-int resistorValue = 10000;
+int resistorValue = 10000.;
 
 // Initialize Timer
 unsigned long startTime;
 unsigned long elapsedTime;
 
-// Initialize Capacitance Variables
-float microFarads;                
-float nanoFarads;
+// Initialize Capacitance Variable               
+float picoFarads;
+
+//Initialize LED 
+int ledPin = 12;
 
 void setup()
 {
-  pinMode(chargePin, OUTPUT);     
+  pinMode(chargePin, OUTPUT);     //cap
   digitalWrite(chargePin, LOW);  
   Serial.begin(9600); // Necessary to print data to serial monitor over USB
+
+  pinMode(ledPin, OUTPUT);  //led
+  digitalWrite(ledPin, LOW);
 }
 
 void loop()
 {
+ 
   digitalWrite(chargePin, HIGH); // Begins charging the capacitor
-  startTime = millis(); // Begins the timer
+  startTime = micros(); // Begins the timer
   
   while(analogRead(analogPin) < 648)
   {       
     // Does nothing until capacitor reaches 63.2% of total voltage
   }
 
-  elapsedTime= millis() - startTime; // Determines how much time it took to charge capacitor
-  microFarads = ((float)elapsedTime / resistorValue) * 1000;
+  elapsedTime= micros() - startTime; // Determines how much time it took to charge capacitor
+  picoFarads = ((float)elapsedTime / resistorValue) * 1000000.0;
   Serial.print(elapsedTime);       
-  Serial.print(" mS    ");         
+  Serial.print(" microS    ");         
+      
+  Serial.print((long)picoFarads);         
+  Serial.println(" picoFarads"); 
 
-  if (microFarads > 1) // Determines if units should be micro or nano and prints accordingly
-  {
-    Serial.print((long)microFarads);       
-    Serial.println(" microFarads");         
-  }
 
-  else
-  {
-    nanoFarads = microFarads * 1000.0;      
-    Serial.print((long)nanoFarads);         
-    Serial.println(" nanoFarads");          
-    delay(500); 
-  }
+//  delay(300);
+//  if (picoFarads>10000){
+//    digitalWrite(ledPin, HIGH);
+//    delay(1000);
+//   
+//  }
+//  digitalWrite(ledPin, LOW);         
+  delay(500); 
+
 
   digitalWrite(chargePin, LOW); // Stops charging capacitor
   pinMode(dischargePin, OUTPUT); 
